275ffaf5c054fd6de51c0926ea2751f2d4fbc597
Benjamin Marwell <bmarwell@gmail.com>
2018-08-02 22:59:36 +0200

    new vue project for a tracker.

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Tracker from '@/components/Tracker';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Tracker',
+      component: Tracker,
+    },
+  ],
+});
